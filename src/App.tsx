f436ba048757c7cd9cba350b0b12e328166a2332
Rob Avery <robertaveryiv@gmail.com>
2024-04-18 00:15:42 -0400

    Added filters for ads and offline

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Grid } from '@mui/material';
-import RadioGroup from '@mui/joy/RadioGroup';
-import Radio from '@mui/joy/Radio';
-import FormControl from '@mui/joy/FormControl';
-import FormHelperText from '@mui/joy/FormHelperText';
-import Card from '@mui/joy/Card';
-import List from '@mui/joy/List';
-import ListItem from '@mui/joy/ListItem';
-import Divider from '@mui/material/Divider';
+import { RadioGroup, Radio, FormControl, 
+  FormHelperText, Card, List, ListItem, 
+  Switch, Typography, Divider } from '@mui/joy';
 import streamers from './assets/streamers.json';
 
 interface PlansSelected {
@@ -20,6 +15,8 @@ interface PlansSelected {
 }
 
 function App() {
+  const [adsFilter, setAdsFilter] = useState(false);
+  const [offlineFilter, setOfflineFilter] = useState(false);
   const [total, setTotal] = useState(0.0);
   const [plans, setPlans] = useState<PlansSelected>({});
 
@@ -36,31 +33,50 @@ function App() {
     setPlans((prevPlans) => ({...prevPlans, ...streamer}))
   }
 
+  const toggleAdsFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAdsFilter(!adsFilter);
+  }
+
+  const toggleOfflineFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setOfflineFilter(!offlineFilter);
+  }
+
   return (
     <div className="App">
       <div className="App-Title">
         <h1>Streamer Guide</h1>
         <h6>Compare and calculate monthly costs of streaming services</h6>
       </div>
+      <div className="App-Filters">
+        <Typography textColor="white" endDecorator={<Switch sx={{ ml: 1 }} onChange={toggleAdsFilter}/>}>
+          Ads-free
+        </Typography>
+        <Divider inset="none" orientation="vertical" sx={{ bgcolor: "white", m: 1 }} />
+        <Typography textColor="white" endDecorator={<Switch sx={{ ml: 1 }} onChange={toggleOfflineFilter}/>}>
+          Only Offline
+        </Typography>
+      </div>
       <Grid container spacing={12}>
+        {/* Cost Breakdown - left pane */}
         <Grid item xs={6} md={3}>
           <div className="Total-Cost">
+            <div className="Total-Cost-Header">Total Cost:</div>
+            <div className="Total-Cost-Price">${total.toFixed(2)}</div>
             {Object.keys(plans).length > 0 && (
               <>
-                <div className="Total-Cost-Header">Breakdown</div>
+                <Divider inset="none" sx={{ bgcolor: "white" }} />
+                {/* <div className="Total-Cost-Header">Breakdown</div> */}
                 {Object.entries(plans).map(([key, value]) => (
                     <div className="Total-Cost-Price">
                       <img className="Total-Cost-Logo" src={value.logo} width="15" height="15"/>
                        {key} - {value.plan} : ${value.price}
                     </div>
                 ))}
-                <Divider variant="middle" sx={{ bgcolor: "white" }} />
               </>
             )}
-            <div className="Total-Cost-Header">Total Cost:</div>
-            <div className="Total-Cost-Price">${total.toFixed(2)}</div>
           </div>
         </Grid>
+        {/* List of streaming services - right pane */}
         <Grid item xs={6} md={9}>
           {streamers.map((streamer) => 
             <Card variant="soft" size="lg" className="Streamer-Card">
@@ -69,7 +85,9 @@ function App() {
                 <div className="Streamer-Card-Title-Text">{streamer.title}</div>
               </div>
               <RadioGroup orientation="horizontal" name="tiers" sx={{ gap: 1, '& > div': { p: 1 } }}>
-                {streamer.plans?.map((plan) => 
+                {streamer.plans?.filter((plan) => offlineFilter ? plan.offline : true)
+                  .filter((plan) => adsFilter ? !plan.ads : true)
+                  .map((plan) => 
                 <FormControl>
                   <Radio className="Streamer-Card-Plan-Title" overlay 
                     value={JSON.stringify({
